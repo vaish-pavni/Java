e97f170712796d5e61a12efa6e71b1afd6ed31a5
vaish-pavni <vaishhpavani@gmail.com>
2026-09-22 19:54:42 +0530

    GUI updated program.

--- GUI.java
+++ GUI.java
@@ -1,7 +1,7 @@
 import javax.swing.JOptionPane;
 public class GUI {
     public static void main(String[] args) {
-        String name = JOptionPane.showInputDialog("Enter your name:");
+        String name = JOptionPane.showInputDialog("Enter name:");
         JOptionPane.showMessageDialog(null, "HELLO"+name);
     }
 }
