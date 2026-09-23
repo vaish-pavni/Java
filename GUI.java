3110799cf89bc545b4e023aa93afde1bd6617cd1
vaish-pavni <vaishhpavani@gmail.com>
2026-09-23 22:11:10 +0530

    GUI updated program.

--- GUI.java
+++ GUI.java
@@ -3,5 +3,8 @@ public class GUI {
     public static void main(String[] args) {
         String name = JOptionPane.showInputDialog("Enter name:");
         JOptionPane.showMessageDialog(null, "HELLO"+name);
+        
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter age:"));
+        JOptionPane.showMessageDialog(null,"You are "+age+"years old");
     }
 }
